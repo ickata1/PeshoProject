0b52e98f25223bcef4639357dfbbafb5852a316c
Dimitar Kazakov <mitparody@gmail.com>
2020-05-20 17:39:40 +0300

    Added missing files

---
+++ PresetsDemo/PresetsDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PresetsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
