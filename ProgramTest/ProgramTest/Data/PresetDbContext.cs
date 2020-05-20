471fb5714d8ecbc44c44ce629b919fced168b40d
Dimitar Kazakov <mitparody@gmail.com>
2020-05-21 02:39:27 +0300

    Fix for the Database

---
+++ ProgramTest/ProgramTest/Data/PresetDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Data.Entities;
+using System;
+using System.Data.Entity;
+
+namespace Data
+{
+    public class PresetDbContext : DbContext
+    {
+        public PresetDbContext() : base("name=PresetsDb")
+        {
+
+        }
+        public virtual DbSet<Preset> Presets { get; set; }
+        public virtual DbSet<PresetSetting> PresetSettings { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Preset>().HasMany<PresetSetting>(preset => preset.PresetSettings); //Probably doe
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ProgramTest/ProgramTest/Data/Repositories/PresetRepository.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data.Repositories
+{
+    public class PresetRepository : CrudRepository<Preset>
+    {
+        public PresetRepository(PresetDbContext dbContext) : base(dbContext,dbContext.Presets)
+        {
+
+        }
+
+        
+    }
+}
