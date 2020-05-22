e5ea93ff042be10198572205688d0bf2776e7858
ickata1 <ickata1@hotmail.com>
2020-05-22 10:15:37 +0300

    Add a sqlite DB file and a DB context

---
+++ ProgramTest/ProgramTest/Data/LocalPresetDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTest.Data
+{
+    class LocalPresetDbContext : DbContext
+    {
+        public LocalPresetDbContext() : base("name=LocalPresetDb")
+        {
+
+        }
+
+        public virtual DbSet<Preset> Presets { get; set; }
+        public virtual DbSet<PresetSetting> PresetSettings { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Conventions
+            .Remove<PluralizingTableNameConvention>();
+
+            //One-to-Many Relationship
+            modelBuilder.Entity<Preset>()
+                .HasMany<PresetSetting>(preset => preset.PresetSettings)
+                .WithRequired(presetSetting => presetSetting.Preset)
+                .HasForeignKey<int>(presetSetting => presetSetting.PresetId)
+                .WillCascadeOnDelete();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
