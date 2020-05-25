3be2d7895003031f921dc09a2cb68bbc8e15ab3a
ickata1 <ickata1@hotmail.com>
2020-05-25 23:44:45 +0300

    Changed Setting Value from string to int

--- ProgramTest/ProgramTest/Data/Entities/Setting.cs
+++ ProgramTest/ProgramTest/Data/Entities/Setting.cs
@@ -11,6 +11,6 @@ namespace ProgramTest.Data.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Value { get; set; }
+        public int Value { get; set; }
     }
 }
