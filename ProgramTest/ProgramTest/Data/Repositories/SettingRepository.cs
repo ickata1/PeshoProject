70d248282847bc59eec3e7f4f6d6792c75c941df
ickata1 <ickata1@hotmail.com>
2020-05-25 23:27:05 +0300

    Added Settings table to the DB

---
+++ ProgramTest/ProgramTest/Data/Repositories/SettingRepository.cs
@@ -0,0 +1,24 @@
+﻿using Data;
+using Data.Repositories;
+using ProgramTest.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTest.Data.Repositories
+{
+    class SettingRepository : CrudRepository<Setting>
+    {
+        public SettingRepository(PresetDbContext dbContext) : base(dbContext, dbContext.Settings)
+        {
+        
+        }
+
+        public Setting GetSettingByName(string name)
+        {
+            return this.GetOne(s => s.Name == name);
+        }
+    }
+}
