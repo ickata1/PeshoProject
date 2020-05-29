636daffb5dd7355d21977dc726e30d5d7e8e982d
Dimitar Dyulgerov <mitko937@gmail.com>
2020-05-29 14:55:11 +0300

    hotfix

--- WindowsBGChanger/PresetterTests/AppManagerTests.cs
+++ WindowsBGChanger/PresetterTests/AppManagerTests.cs
@@ -28,9 +28,6 @@ namespace PresetterTests
             Assert.AreEqual(-1, result);
         }
 
-        //TODO...
-        //Check if it changes the bg by comparing wallpaper file pahts!
-
 
         [TestMethod]
         public void OpenLink_PassValidUrl_ReturnsTrue()
