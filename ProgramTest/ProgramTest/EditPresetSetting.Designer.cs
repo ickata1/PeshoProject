5cd1c187b875761ce2dfb1fa2f510181f2ec2421
Dimitar Dyulgerov <mitko937@gmail.com>
2020-05-29 15:25:44 +0300

    we HAWT fix

--- ProgramTest/ProgramTest/EditPresetSetting.Designer.cs
+++ ProgramTest/ProgramTest/EditPresetSetting.Designer.cs
@@ -141,6 +141,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.ClientSize = new System.Drawing.Size(214, 184);
             this.Controls.Add(this.presetType);
             this.Controls.Add(this.valueTextBox);
